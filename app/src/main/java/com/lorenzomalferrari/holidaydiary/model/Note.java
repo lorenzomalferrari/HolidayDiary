03a1579d660d2ba8985493b0bcc0781d4b443cb1
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-11 15:14:38 +0200

    Implement Note.java

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Note.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Note.java
@@ -1,4 +1,38 @@
 package com.lorenzomalferrari.holidaydiary.model;
 
 public class Note {
+
+    String title,description;
+
+    public Note() {
+    }
+
+    public Note(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
