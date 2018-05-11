6d503cc70733ec5a96f375b0e1336a00cd8085d7
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-11 10:23:25 +0200

    Update

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/TravelsFragment.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/TravelsFragment.java
@@ -1,7 +1,5 @@
 package com.lorenzomalferrari.holidaydiary;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,7 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class TravelsFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
