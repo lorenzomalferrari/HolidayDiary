f7c7d9f38cd46eb52714950d5539b8c40d97c6b3
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-11 10:22:29 +0200

    Update

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Travel.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Travel.java
@@ -1,4 +1,71 @@
 package com.lorenzomalferrari.holidaydiary.model;
 
 public class Travel {
+
+    private String title;
+    private String Category;
+    private String Description;
+    private int Thumbnail;
+
+    /**
+     *
+     */
+    public Travel() {
+    }
+
+    /**
+     *
+     * @param title
+     * @param category
+     * @param description
+     * @param thumbnail
+     */
+    public Travel(String title, String category, String description, int thumbnail) {
+        this.title = title;
+        Category = category;
+        Description = description;
+        Thumbnail = thumbnail;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return Category;
+    }
+
+    public void setCategory(String category) {
+        Category = category;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public int getThumbnail() {
+        return Thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        Thumbnail = thumbnail;
+    }
+
+    @Override
+    public String toString() {
+        return "Travel{" +
+                "title='" + title + '\'' +
+                ", Category='" + Category + '\'' +
+                ", Description='" + Description + '\'' +
+                ", Thumbnail=" + Thumbnail +
+                '}';
+    }
 }
