70e987d46be39dcdacf266812914a2bdc66a41fd
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-14 15:48:57 +0200

    Add attributes and methods

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Place.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Place.java
@@ -2,9 +2,118 @@ package com.lorenzomalferrari.holidaydiary.model;
 
 public class Place {
 
-    private int id;
-    private String name;
-    private int latitude,Longitude;
-    private String city, country;
-    private int id_user, id_picture, id_travel, id_note;
+    private int id, latitude,longitude, id_user, id_picture, id_travel, id_note;
+    private String name, city, country;
+
+    public Place() {
+    }
+
+    public Place(int id, String name, int latitude, int longitude, String city, String country, int id_user, int id_picture, int id_travel, int id_note) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.city = city;
+        this.country = country;
+        this.id_user = id_user;
+        this.id_picture = id_picture;
+        this.id_travel = id_travel;
+        this.id_note = id_note;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+
+    public int getId_picture() {
+        return id_picture;
+    }
+
+    public void setId_picture(int id_picture) {
+        this.id_picture = id_picture;
+    }
+
+    public int getId_travel() {
+        return id_travel;
+    }
+
+    public void setId_travel(int id_travel) {
+        this.id_travel = id_travel;
+    }
+
+    public int getId_note() {
+        return id_note;
+    }
+
+    public void setId_note(int id_note) {
+        this.id_note = id_note;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Place{" +
+                "id=" + id +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", id_user=" + id_user +
+                ", id_picture=" + id_picture +
+                ", id_travel=" + id_travel +
+                ", id_note=" + id_note +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
