407273a16a7906b9611dc542383bea90927f8532
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-14 12:49:43 +0200

    Add new class

---
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Place.java
@@ -0,0 +1,10 @@
+package com.lorenzomalferrari.holidaydiary.model;
+
+public class Place {
+
+    private int id;
+    private String name;
+    private int latitude,Longitude;
+    private String city, country;
+    private int id_user, id_picture, id_travel, id_note;
+}
