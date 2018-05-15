433880ed5d670d44c7b75424ef663b3b6241e1d4
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-15 10:49:39 +0200

    New class

---
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Picture.java
@@ -0,0 +1,14 @@
+package com.lorenzomalferrari.holidaydiary.model;
+
+public class Picture {
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
