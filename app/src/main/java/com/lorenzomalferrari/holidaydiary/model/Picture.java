3e6a4e28b5399c43e96a82c7ad67aea126ed82d0
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-15 12:29:55 +0200

    Add attribute

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Picture.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Picture.java
@@ -3,15 +3,16 @@ package com.lorenzomalferrari.holidaydiary.model;
 public class Picture {
 
     private int id, id_user;
-    private String title, description;
+    private String title, description, path;
 
     public Picture() {
     }
 
-    public Picture(int id, String title, String description, int id_user) {
+    public Picture(int id, String title, String description, String path, int id_user) {
         this.id = id;
         this.title = title;
         this.description = description;
+        this.path = path;
         this.id_user = id_user;
     }
 
@@ -47,6 +48,14 @@ public class Picture {
         this.description = description;
     }
 
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
     @Override
     public String toString() {
         return "Picture{" +
@@ -54,6 +63,7 @@ public class Picture {
                 ", id_user=" + id_user +
                 ", title='" + title + '\'' +
                 ", description='" + description + '\'' +
+                ", path='" + path + '\'' +
                 '}';
     }
 }
