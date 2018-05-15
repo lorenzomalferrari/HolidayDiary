3b7ebb8b87bb9373c4e17ee2bc9f80cac0e08976
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-15 11:00:27 +0200

    Implements class

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Picture.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Picture.java
@@ -2,7 +2,34 @@ package com.lorenzomalferrari.holidaydiary.model;
 
 public class Picture {
 
-    private String title;
+    private int id, id_user;
+    private String title, description;
+
+    public Picture() {
+    }
+
+    public Picture(int id, String title, String description, int id_user) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.id_user = id_user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
 
     public String getTitle() {
         return title;
@@ -10,5 +37,23 @@ public class Picture {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    @Override
+    public String toString() {
+        return "Picture{" +
+                "id=" + id +
+                ", id_user=" + id_user +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
