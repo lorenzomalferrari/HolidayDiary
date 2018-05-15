4220e31b5923bfb161daf12b5dfce813b29b4e01
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-15 17:10:27 +0200

    Implemented class

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/AddPictureActivity.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/AddPictureActivity.java
@@ -1,12 +1,25 @@
 package com.lorenzomalferrari.holidaydiary;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.lorenzomalferrari.holidaydiary.control.DatabaseHelper;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class AddPictureActivity extends AppCompatActivity {
 
@@ -14,6 +27,8 @@ public class AddPictureActivity extends AppCompatActivity {
     ImageView img;
     Button chooseImg, saveImg;
     DatabaseHelper databaseHelper;
+
+    final int REQUEST_CODE_GALLERY = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +40,54 @@ public class AddPictureActivity extends AppCompatActivity {
         //inizializzo l'oggetto DatabaseHelper
         //il parametro è la classe nella quale inizializzo l'oggetto
         databaseHelper = new DatabaseHelper(this);
+
+        chooseImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(
+                        AddPictureActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY
+                );
+            }
+        });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_CODE_GALLERY){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,REQUEST_CODE_GALLERY);
+            }
+            else {
+                Toast.makeText(getApplicationContext(),"Tu non hai i permessi per accedere",Toast.LENGTH_LONG).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK && data != null){
+            Uri uri = data.getData();
+
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                img.setBackgroundResource(R.color.trasparent);
+                img.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     /**
      * Metodo che mi inizializza gli attributi
