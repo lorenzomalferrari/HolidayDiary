66f738cb782c1848333a71d7f30f6005531a4a6f
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-15 12:53:23 +0200

    Deleted comment

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/MenuActivity.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/MenuActivity.java
@@ -125,12 +125,10 @@ public class MenuActivity extends AppCompatActivity
                 //fragment = new HomePageFragment();
                 break;
             case R.id.nav_travels:
-                //fragment = new TravelsFragment();
                 intent = new Intent(this, TravelsActivity.class);
                 this.startActivity(intent);
                 break;
             case R.id.nav_notes:
-                //fragment = new NotesFragment();
                 intent = new Intent(this, NotesActivity.class);
                 this.startActivity(intent);
                 break;
