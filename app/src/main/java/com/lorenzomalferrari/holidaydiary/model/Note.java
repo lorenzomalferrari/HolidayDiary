98f8c94b8b0224e4f018deb00bcfda3b2c046cad
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-16 15:08:34 +0200

    renamed attribute from content to description

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Note.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Note.java
@@ -7,7 +7,7 @@ public class Note {
     // Attributi della tabella Notes presente sul database HolidayDiary.db
     private int id;
     private String title;
-    private String content;
+    private String description;
     private Date creation_date;
     private int id_user, id_travel, id_place, id_picture;
 
@@ -17,7 +17,7 @@ public class Note {
     public Note() {
         this.id = 1;
         this.title = "Nota 1";
-        this.content = "Questo è il testo della prima nota";
+        this.description = "Questo è il testo della prima nota";
         this.creation_date = new Date();//data corrente
         this.id_user = 1;
         this.id_travel = 1;
@@ -25,22 +25,26 @@ public class Note {
         this.id_place = 1;
     }
 
+    public Note(String title, Date creation_date, String description) {
+        this.title = title;
+        this.description = description;
+        this.creation_date = creation_date;
+    }
+
     /**
      * Costruttore parametrico
      * Inizializza l'oggetto con i parametri passati
-     * @param id
      * @param title
-     * @param content
+     * @param description
      * @param creation_date
      * @param id_user
      * @param id_travel
      * @param id_place
      * @param id_picture
      */
-    public Note(int id, String title, String content, Date creation_date, int id_user, int id_travel, int id_picture, int id_place) {
-        this.id = id;
+    public Note(String title, String description, Date creation_date, int id_user, int id_travel, int id_picture, int id_place) {
         this.title = title;
-        this.content = content;
+        this.description = description;
         this.creation_date = creation_date;
         this.id_user = id_user;
         this.id_travel = id_travel;
@@ -64,12 +68,12 @@ public class Note {
         this.title = title;
     }
 
-    public String getContent() {
-        return content;
+    public String getDescription() {
+        return description;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Date getCreation_date() {
@@ -121,7 +125,7 @@ public class Note {
         return "Note{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
+                ", description='" + description + '\'' +
                 ", creation_date=" + creation_date +
                 ", id_user=" + id_user +
                 ", id_travel=" + id_travel +
