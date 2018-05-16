ac7694e7c3c0a969481ee6fcc6fcd144a2b76982
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-16 10:55:28 +0200

    Added attributes

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/control/Controller.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/control/Controller.java
@@ -1,8 +1,19 @@
 package com.lorenzomalferrari.holidaydiary.control;
 
+import com.lorenzomalferrari.holidaydiary.model.Note;
+import com.lorenzomalferrari.holidaydiary.model.Picture;
+import com.lorenzomalferrari.holidaydiary.model.Place;
+import com.lorenzomalferrari.holidaydiary.model.Travel;
 import com.lorenzomalferrari.holidaydiary.model.User;
+import com.lorenzomalferrari.holidaydiary.model.Validator;
 
 public class Controller {
 
     User user;
+    Note note;
+    Picture picture;
+    Place place;
+    Travel travel;
+    Validator validator;
+
 }
