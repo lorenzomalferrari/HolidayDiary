f95a4d62ce91cbe3675881bc18f0534b8a4936fa
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-24 15:44:37 +0200

    Settings changed

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Validator.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Validator.java
@@ -22,7 +22,12 @@ public class Validator {
         // Set attributes
     }
 
-    //Controllo che i campi obbligatori siano pieni
+    /**
+     * Controllo che i campi email e password non siano vuoti
+     * @param email
+     * @param password
+     * @return true se non sono vuoti, false se sono vuoti
+     */
     public boolean isFieldsEmpty(String email, String password){
         if (email.trim().length() > 0 && password.trim().length() > 0)
         {
@@ -44,7 +49,8 @@ public class Validator {
         //Array dei char di lastName
         String[] lastNameSplit = lastName.split("");
         //boolean var
-        boolean flag = false;
+        boolean flag_firstName = false;
+        boolean flag_lastName = false;
 
         //Controllo firstName
         for (int i = 0; i <= firstNameSplit.length; i ++){
@@ -54,9 +60,9 @@ public class Validator {
             }
             catch(NumberFormatException nfe)
             {
-                flag = false;
+                flag_firstName = false;
             }
-            flag = true;
+            flag_firstName = true;
         }
 
         //Controllo lastName
@@ -67,14 +73,23 @@ public class Validator {
             }
             catch(NumberFormatException nfe)
             {
-                flag = false;
+                flag_lastName = false;
             }
-            flag = true;
+            flag_lastName = true;
         }
-        return flag;
+        return (flag_firstName && flag_lastName);
     }
 
     //Controllo username
+    /**
+     * Controllo che il campo username non sia vuoto
+     * @param username
+     * @return true se non è vuoto || false se è campo è vuoto
+     */
+    public boolean isUsernameEmpty(String username){
+        if (username.trim().length() > 0) return true;
+        else return false;
+    }
 
     //Controllo email
     /**
@@ -104,7 +119,11 @@ public class Validator {
 
     //Controllo compleanno
 
-    //Calcolo età della persona
+    /**
+     * Calcola l'età dell'utente usando come parametro d'ingresso la data di nascita
+     * @param data
+     * @return
+     */
     public int calcAge(Date data){
         int age = 0;
         Calendar  myData= Calendar.getInstance();
@@ -115,16 +134,10 @@ public class Validator {
         int day_of_month = today.get(Calendar.DAY_OF_MONTH) - myData.get(Calendar.DAY_OF_MONTH);
         if (year > 0){
             if (month > 0){
-                if (day_of_month > 0){
-                    age = year;
-                }
-                else {
-                    age = year-1;
-                }
+                if (day_of_month > 0) age = year;
+                else age = year - 1;
             }
-            else {
-                age = year-1;
-            }
+            else age = year - 1;
         }
         else {
             //Impossibile, la persona deve ancora nascere
@@ -145,15 +158,7 @@ public class Validator {
         else return true;
     }
 
-    /**
-     * Controllo che il campo username non sia vuoto
-     * @param username
-     * @return true se non è vuoto || false se è campo è vuoto
-     */
-    public boolean isUsernameEmpty(String username){
-        if (username.trim().length() > 0) return true;
-        else return false;
-    }
+
 
     public boolean isBirthdateValid() {
         return false;
