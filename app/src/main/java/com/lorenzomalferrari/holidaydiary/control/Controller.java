040b5a62e26d5aef95744846710a606bb46144ce
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-06-01 12:45:11 +0200

    Deleted import

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/control/Controller.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/control/Controller.java
@@ -1,12 +1,5 @@
 package com.lorenzomalferrari.holidaydiary.control;
 
-import com.lorenzomalferrari.holidaydiary.model.Note;
-import com.lorenzomalferrari.holidaydiary.model.Picture;
-import com.lorenzomalferrari.holidaydiary.model.Place;
-import com.lorenzomalferrari.holidaydiary.model.Travel;
-import com.lorenzomalferrari.holidaydiary.model.User;
-import com.lorenzomalferrari.holidaydiary.model.Validator;
-
 /**
  *
  * @author Lorenzo Malferrari - Website: www.lorenzomalferrari.com
