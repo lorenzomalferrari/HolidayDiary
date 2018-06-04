ff9f4af972967889adcf48430c110815d9bb31c6
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-06-04 15:51:39 +0200

    Added img and created the gallery

---
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/control/MasonryAdapter.java
@@ -0,0 +1,55 @@
+package com.lorenzomalferrari.holidaydiary.control;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.lorenzomalferrari.holidaydiary.R;
+
+/**
+ * Created by Suleiman on 26-07-2015.
+ */
+public class MasonryAdapter extends RecyclerView.Adapter<MasonryAdapter.MasonryView> {
+
+    private Context context;
+
+    int[] imgList = {R.drawable.two, R.drawable.one, R.drawable.three, R.drawable.four,
+            R.drawable.five, R.drawable.six, R.drawable.seven, R.drawable.eight,
+            R.drawable.nine, R.drawable.ten};
+
+    String[] nameList = {"One", "Two", "Three", "Four", "Five", "Six",
+            "Seven", "Eight", "Nine", "Ten"};
+
+    public MasonryAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public MasonryView onCreateViewHolder(ViewGroup parent, int viewType) {
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_item_picture, parent, false);
+        MasonryView masonryView = new MasonryView(layoutView);
+        return masonryView;
+    }
+
+    @Override
+    public void onBindViewHolder(MasonryView holder, int position) {
+        holder.imageView.setImageResource(imgList[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return nameList.length;
+    }
+
+    class MasonryView extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public MasonryView(View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.picture_img_id);
+        }
+    }
+}
--- app/src/main/java/com/lorenzomalferrari/holidaydiary/view/activity/PicturesActivity.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/view/activity/PicturesActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +14,9 @@ import android.widget.Toast;
 
 import com.lorenzomalferrari.holidaydiary.R;
 import com.lorenzomalferrari.holidaydiary.control.DatabaseHelper;
+import com.lorenzomalferrari.holidaydiary.control.MasonryAdapter;
 import com.lorenzomalferrari.holidaydiary.control.PictureListAdapter;
+import com.lorenzomalferrari.holidaydiary.control.SpacesItemDecoration;
 import com.lorenzomalferrari.holidaydiary.model.Picture;
 
 import java.util.ArrayList;
@@ -28,6 +32,7 @@ public class PicturesActivity extends AppCompatActivity {
     PictureListAdapter pictureListAdapter;
     DatabaseHelper databaseHelper;
     Toolbar toolbar;
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +44,14 @@ public class PicturesActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // Setta la var dell'oggetto DatabaseHelper con il context della classe stessa
         databaseHelper = new DatabaseHelper(this);
-        gridView = findViewById(R.id.gridViewPictures_id);
+        //gridView = findViewById(R.id.gridViewPictures_id);
         // Create list of Travel object
-        listPicture = new ArrayList<>();
         //listPicture = new ArrayList<>();
-        pictureListAdapter = new PictureListAdapter(this, R.layout.picture_items, listPicture);
-        gridView.setAdapter(pictureListAdapter);
+        //listPicture = new ArrayList<>();
+        //pictureListAdapter = new PictureListAdapter(this, R.layout.picture_items, listPicture);
+        //gridView.setAdapter(pictureListAdapter);
 
-        // Salvo tutte i Viaggi
+        /*// Salvo tutte i Viaggi
         Cursor cursor = databaseHelper.getAll("Pictures");
         listPicture.clear();
         while (cursor.moveToNext()){
@@ -58,7 +63,15 @@ public class PicturesActivity extends AppCompatActivity {
             //Aggiungo alla lista le immagini
             listPicture.add(new Picture(title,image,id_user));
         }
-        pictureListAdapter.notifyDataSetChanged();
+        pictureListAdapter.notifyDataSetChanged();*/
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.masonry_grid);
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+        MasonryAdapter adapter = new MasonryAdapter(this);
+        mRecyclerView.setAdapter(adapter);
+        SpacesItemDecoration decoration = new SpacesItemDecoration(16);
+        mRecyclerView.addItemDecoration(decoration);
     }
 
     @Override
