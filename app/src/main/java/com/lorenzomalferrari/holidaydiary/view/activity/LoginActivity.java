8c9e92cabe2ce7753e528fb511bdf13cd1d9d1b7
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-06-04 09:13:02 +0200

    Deleted imports

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/view/activity/LoginActivity.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/view/activity/LoginActivity.java
@@ -2,16 +2,11 @@ package com.lorenzomalferrari.holidaydiary.view.activity;
 //Import java classes
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
