e6874969e428e5932f8f3916c5b610abf87bd4a6
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-06-07 12:31:37 +0200

    Implemented code

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/view/activity/AddTravelActivity.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/view/activity/AddTravelActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.lorenzomalferrari.holidaydiary.R;
 import com.lorenzomalferrari.holidaydiary.control.DatabaseHelper;
@@ -20,6 +24,11 @@ public class AddTravelActivity extends AppCompatActivity {
     EditText txttitle, txtcategory, txtdescription;
     Button addTravel;
     DatabaseHelper databaseHelper;
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+
+
+    private String category;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,10 +41,23 @@ public class AddTravelActivity extends AppCompatActivity {
         //Inizializzazione degli attributi
         init();
 
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                category = parent.getItemAtPosition(position).toString();
+                Toast.makeText(AddTravelActivity.this,category,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         addTravel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Travel travel = new Travel(txttitle.getText().toString(),txtdescription.getText().toString(),1);
+                Travel travel = new Travel(txttitle.getText().toString(),category,txtdescription.getText().toString(),1);
                 databaseHelper.insertDataTravel(travel);
                 //Chiamo l'Activity che contiene la lista delle note
                 callTravels();
@@ -46,9 +68,14 @@ public class AddTravelActivity extends AppCompatActivity {
 
     private void init(){
         txttitle = findViewById(R.id.addTravel_title);
-        txtcategory = findViewById(R.id.addTravel_category);
+        //txtcategory = findViewById(R.id.addTravel_category);
         txtdescription = findViewById(R.id.addTravel_description);
         addTravel = findViewById(R.id.addTravel_save_travel);
+        spinner = findViewById(R.id.category_spinner);
+        adapter = ArrayAdapter.createFromResource(this,R.array.category_list,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 
     private void callTravels() {
