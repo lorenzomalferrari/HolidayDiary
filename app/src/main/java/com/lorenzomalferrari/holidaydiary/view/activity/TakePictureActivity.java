a0046462f6f85b3376f2106ca306edcf38fbae31
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-06-11 09:58:23 +0200

    Implemented activity

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/view/activity/TakePictureActivity.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/view/activity/TakePictureActivity.java
@@ -1,13 +1,43 @@
 package com.lorenzomalferrari.holidaydiary.view.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.lorenzomalferrari.holidaydiary.R;
 
 public class TakePictureActivity extends AppCompatActivity {
+
+    Button openCamera;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture);
+
+        openCamera = findViewById(R.id.btnTakePicture);
+        imageView = findViewById(R.id.img_takePicture);
+
+        openCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
     }
 }
