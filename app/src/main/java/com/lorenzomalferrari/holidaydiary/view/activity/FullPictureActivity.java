7c2770eda120c0b51cc23d4d6392ecb7acfb7b23
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-06-12 12:42:53 +0200

    Added activity and Updating

---
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/view/activity/FullPictureActivity.java
@@ -0,0 +1,15 @@
+package com.lorenzomalferrari.holidaydiary.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lorenzomalferrari.holidaydiary.R;
+
+public class FullPictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_picture);
+    }
+}
