eb936e1e2bcff4dbc151cc4fe16c492b2d005c6a
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-06-13 17:19:41 +0200

    Updating title

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/view/fragment/PrivacyTermsFragment.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/view/fragment/PrivacyTermsFragment.java
@@ -30,6 +30,6 @@ public class PrivacyTermsFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
-        getActivity().setTitle("privacytermsofuse");
+        getActivity().setTitle(R.string.privacyterms_title);
     }
 }
