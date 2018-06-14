4323e8863773eda2d152dc96b94a45a5e64f8309
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-06-14 15:59:02 +0200

    Updating for presentation

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/view/fragment/HomePageFragment.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/view/fragment/HomePageFragment.java
@@ -8,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.lorenzomalferrari.holidaydiary.R;
 
@@ -33,5 +35,23 @@ public class HomePageFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle(R.string.home_page_title);
+
+        ImageView imageView = view.findViewById(R.id.travel_img_id);
+        TextView textView = view.findViewById(R.id.travel_title_id);
+        imageView.setImageResource(R.drawable.bicifiori);
+        imageView.setMaxHeight(100);
+        textView.setText("Sardegna");
+
+        TextView textViewnota = view.findViewById(R.id.note_title_id);
+        textViewnota.setText("Spesa");
+        TextView textViewdatanota = view.findViewById(R.id.note_data_id);
+        textViewdatanota.setText("21/04/2018");
+
+        TextView textViewnota2 = view.findViewById(R.id.note_title_id);
+        textViewnota.setText("Spesa");
+        TextView textViewdatanota2 = view.findViewById(R.id.note_data_id);
+        textViewdatanota.setText("21/04/2018");
+
+
     }
 }
